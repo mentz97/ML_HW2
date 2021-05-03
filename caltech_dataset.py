9d5e59830792ab301c1f33d7b5787e49b5c9cb52
mentz <francesco.menon@live.it>
2021-05-03 12:17:02 +0200

    dd

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -41,7 +41,7 @@ class Caltech(VisionDataset):
           path=path.split('/')
           if(path[0] != "BACKGROUND_Google"):
             images_paths.append(self.root + '/' + tmp)
-            labels.append(self.categories.index[path[0]])
+            labels.append(self.categories.index(path[0]))
 
         self.data = pd.DataFrame(zip(images_paths, labels), columns = ["image_path", "label"])
         
